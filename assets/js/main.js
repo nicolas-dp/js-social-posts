03447a9fb6d8ee1d93116e405d5f9d41ca68d125
Nicolas <wellcomenicolas@gmail.com>
2022-04-12 16:59:57 +0200

    Stamp element in DOM

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,8 @@ Ricreiamo un feed social aggiungendo al layout di base fornito, il nostro script
 
 
 //**** MILESTONE 1
-Creiamo il nostro array di oggetti che rappresentano ciascun post. Ogni post dovrà avere le informazioni necessarie 
-per stampare la relativa card:
+Creiamo il nostro array di oggetti che rappresentano ciascun post. Ogni 
+post dovrà avere le informazioni necessarie per stampare la relativa card:
 id del post, 
 numero progressivo da 1 a n
 nome autore,
@@ -23,10 +23,10 @@ const posts = [
         id_post: "first_post",
         n: 1,
         nome_autore: "Phil Mangione",
-        image_avatar: "https://unsplash.it/70/70?image=<id>",
+        image_avatar: `<img src="https://picsum.photos/70">`,
         data: "25/6/2021",
         paragraph: "Lorem ipsum dolor sit amet, consectetur adipisicing elit",
-        image_post: "https://unsplash.it/300/300?image=<id>",
+        image_post: `<img src=https://picsum.photos/id/237/200/300>`,
         n_of_like: 80,
     },
 
@@ -34,12 +34,37 @@ const posts = [
         id_post: "second_post",
         n: 2,
         nome_autore: "Sofia Perlari",
-        image_avatar: "https://unsplash.it/70/70?image=<id>",
+        image_avatar: `<img src="https://picsum.photos/70">`,
         data: "3/9/2021",
         paragraph: "Lorem ipsum dolor sit amet, consectetur adipisicing elit",
-        image_post: "https://unsplash.it/300/300?image=<id>",
+        image_post: `<img src=https://picsum.photos/id/237/200/300>`,
         n_of_like: 120,
     },
 ]
 
 
+/* MILESTONE 2
+Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post del nostro feed. */
+
+const elementoDom = document.querySelector(".card")
+
+
+
+posts.forEach(element => {
+    const createMarkup = `<div class="card_sup">
+        ${element.image_avatar}
+        <h5>${element.nome_autore}</h5>
+        <em>${element.data}</em>
+    </div>
+
+    <div class="card_body">
+        <p>${element.data}</p>
+        ${element.image_post}
+        <div class="like">Mi Piace</div>
+        <div class="number_like">Piace a ${element.n_of_like} persone</div>
+    </div>`
+    elementoDom.insertAdjacentHTML("beforeend", createMarkup)
+
+    console.log(createMarkup)
+
+})
