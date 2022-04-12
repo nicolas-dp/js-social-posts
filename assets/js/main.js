66e49517fd2a53521e2dc0ce252f31d208288d19
Nicolas <wellcomenicolas@gmail.com>
2022-04-12 16:22:39 +0200

    Add array of object

---
+++ assets/js/main.js
@@ -0,0 +1,45 @@
+/* Descrizione
+Ricreiamo un feed social aggiungendo al layout di base fornito, il nostro script JS in cui:
+
+
+//**** MILESTONE 1
+Creiamo il nostro array di oggetti che rappresentano ciascun post. Ogni post dovrà avere le informazioni necessarie 
+per stampare la relativa card:
+id del post, 
+numero progressivo da 1 a n
+nome autore,
+foto autore,
+data in formato americano (mm-gg-yyyy),
+testo del post,
+immagine (non tutti i post devono avere una immagine),
+numero di likes.
+Non è necessario creare date casuali
+Per le immagini va bene utilizzare qualsiasi servizio di placeholder ad es. Unsplash (https://unsplash.it/300/300?image=<id>) */
+
+
+//Creiamo il nostro array di oggetti che rappresentano ciascun post.
+const posts = [
+    {
+        id_post: "first_post",
+        n: 1,
+        nome_autore: "Phil Mangione",
+        image_avatar: "https://unsplash.it/70/70?image=<id>",
+        data: "25/6/2021",
+        paragraph: "Lorem ipsum dolor sit amet, consectetur adipisicing elit",
+        image_post: "https://unsplash.it/300/300?image=<id>",
+        n_of_like: 80,
+    },
+
+    {
+        id_post: "second_post",
+        n: 2,
+        nome_autore: "Sofia Perlari",
+        image_avatar: "https://unsplash.it/70/70?image=<id>",
+        data: "3/9/2021",
+        paragraph: "Lorem ipsum dolor sit amet, consectetur adipisicing elit",
+        image_post: "https://unsplash.it/300/300?image=<id>",
+        n_of_like: 120,
+    },
+]
+
+
